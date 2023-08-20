71dce7c3d1f2e6f7bb6373305e338c772b97e48f
Silence-tss <99872463+Silence-tss@users.noreply.github.com>
2023-08-21 00:08:57 +0300

    Task-1, proba 1

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categoryLengthEl = document.querySelectorAll('.item');
+console.log('Number of categories:', categoryLengthEl.length);
+console.log(categoryLengthEl);
+// const listCategoryMenuEl = 
+categoryLengthEl.forEach(function (category, index) {
+    console.log(categoryLengthEl);
+});
