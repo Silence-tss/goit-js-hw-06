f63c793c606e98944c1af275c89d3fef76310cf3
Silence-tss <99872463+Silence-tss@users.noreply.github.com>
2023-08-21 22:42:36 +0300

    Update task-1.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,34 @@
 const categoryLengthEl = document.querySelectorAll('.item');
 console.log('Number of categories:', categoryLengthEl.length);
-console.log(categoryLengthEl);
-// const listCategoryMenuEl = 
-categoryLengthEl.forEach(function (category, index) {
-    console.log(categoryLengthEl);
+
+const navArray = document.querySelector('#categories');
+const listNavArray = navArray.children;
+console.log(listNavArray);
+
+listNavArray.forEach(function (name, index, array) {
+    const navElement = document.querySelector('.item');
+    const firstEl = navElement.firstElementChild;
+    console.log('Category:', firstEl.textContent);
+
 });
+
+// const navElement = document.querySelector('.item');
+// const firstEl = navElement.firstElementChild;
+// console.log('Category:', firstEl.textContent);
+
+// const newClassNavEl = navElement.lastElementChild;
+// newClassNavEl.classList.add('child-item');
+
+// const listNavEl = newClassNavEl.children;
+// console.log('Elements:', listNavEl.length);
+// })
+
+// const navElement = document.querySelector('.item');
+// const firstEl = navElement.firstElementChild;
+// console.log('Category:', firstEl.textContent);
+
+// const newClassNavEl = navElement.lastElementChild;
+// newClassNavEl.classList.add('child-item');
+
+// const listNavEl = newClassNavEl.children;
+// console.log('Elements:', listNavEl.length);
